70d2c2ce027a363c76da688d0d5490d23570d404
Daria Volkova <dvolkova@leasehawk.com>
2021-08-13 19:38:09 +0300

    deploy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PrimeNGConfig } from "primeng/api";
+import {ThemeService} from "./services/theme.service";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'test-app';
+export class AppComponent implements OnInit {
+
+  constructor(private primengConfig: PrimeNGConfig,
+              public themeService: ThemeService) {}
+
+  public ngOnInit(): void {
+    this.primengConfig.ripple = true;
+  }
+
+  public changeTheme(name: string): void {
+    this.themeService.switchTheme(name);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { ButtonModule } from 'primeng/button';
+import { ThemeService } from "./services/theme.service";
+import { CardModule } from 'primeng/card';
+import { DividerModule } from 'primeng/divider';
 
 @NgModule({
   declarations: [
@@ -10,9 +15,13 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    ButtonModule,
+    CardModule,
+    DividerModule
   ],
-  providers: [],
+  providers: [ThemeService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
